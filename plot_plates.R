c6c09c1f60cfac0fde2a400dd3aa85034391f050
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-11-08 22:09:22 +0100

    refactor: apply dplyr style

--- plot_plates.R
+++ plot_plates.R
@@ -1,74 +1,80 @@
+# Copyright 2018 Novo Nordisk Foundation Center for Biosustainability,
+# Technical University of Denmark.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Libraries ---------------------------------------------------------------
+
+library(readr)
+library(dplyr)
 library(ggplot2)
-library(tidyverse)
-data <- read.csv('2018_0817_Wt_131.csv')
-data_reduced <- select(data, Well.Name, All.Events..Events, All.Events.FITC.A.Mean ) #3 columns out of 108 that I am interested in
-names(data_reduced) <- c('Well', 'count', 'Mean_fluorescence') # Rename the columns
 
-head(data_reduced)
+# Function Definitions ----------------------------------------------------
 
-data_reduced$Mean_fluorescence[data_reduced$count < 5000] <- NA    # Remove wells that have less than 5000 cells
+extract_transform_load <- function(filename, threshold = 5000) {
+  my_tbl <- readr::read_csv(filename) %>%
+    dplyr::select(
+      well = `Well Name`,
+      count = `All Events #Events`,
+      mean_fluorescence = `All Events FITC-A Mean`
+    ) %>%
+    dplyr::mutate(
+      plate = factor(rep(c("A", "B"), times = 3, each = 96)),
+      ligand = factor(rep(c("None", "VAC", "Vanillin"), times = 2, each = 96)),
+      type = "Mutant",
+      type = factor(replace(type, well %in% c("H11", "H12"), "Control")),
+    )
+    # ) %>%
+    # dplyr::filter(count >= threshold)
+  return(my_tbl)
+}
 
-Both_plates <- data_reduced
-Both_plates[1:96, 'No_ligand'] <- data_reduced$Mean_fluorescence[1:96]           #Plate_A_No_Ligand
-Both_plates[1:96, 'VAC'] <- data_reduced$Mean_fluorescence[193:288]      #Plate_A_VAC
-Both_plates[1:96, 'Vanillin'] <- data_reduced$Mean_fluorescence[385:480]     #Plate_A_Vanillin
+# Command Line Parsing ----------------------------------------------------
 
-Both_plates[97:192, 'No_ligand'] <- data_reduced$Mean_fluorescence[97:192]      #Plate_B_No_Ligand
-Both_plates[97:192, 'VAC'] <- data_reduced$Mean_fluorescence[289:384]     #Plate_B_VAC
-Both_plates[97:192, 'Vanillin'] <- data_reduced$Mean_fluorescence[481:576]     #Plate_B_Vanillin
-Both_plates <- Both_plates[1:192,]
-Both_plates <- select(Both_plates, -count, -Mean_fluorescence)
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 1) {
+  stop("Expected one command line argument specifying the CSV data file.")
+}
 
-Both_plates[1:192, 'Type'] <- 'Mutant'       # Add an extra variable
-Both_plates[95:96, 'Type'] <- 'Control'
-Both_plates[191:192, 'Type'] <- 'Control'
-#Both_plates[c(95:96, 191:192), 'Type'] <- 'Control'
+# Load Data ---------------------------------------------------------------
 
-Both_plates$Well <- as.character(Both_plates$Well)
-Both_plates$Well[1:96] <- paste0('Plate_1_', Both_plates$Well[1:96])
-Both_plates$Well[97:192] <-  paste0('Plate_2_', Both_plates$Well[97:192])
+my_df <- extract_transform_load(args[1])
 
+# Plot Data ---------------------------------------------------------------
 
-myplot_1 <- gather(Both_plates,  No_ligand, VAC, Vanillin, key = 'ligand', value = 'induction' ) #myplot_1 is all raw data
-myplot_1 <- ggplot(myplot_1, aes(x= Well, color = interaction(ligand, Type)))
-myplot_1 + geom_point(aes(y= induction), size = 3 ) + facet_grid(. ~ ligand) 
+ggplot(my_df, aes(x = mean_fluorescence,
+                  fill = type)) +
+  geom_histogram(position = "dodge") +
+  facet_grid(. ~ ligand) +
+  scale_fill_brewer(palette = "Set1")
 
+ggplot(my_df, aes(x = type,
+                  y = mean_fluorescence,
+                  color = type)) +
+  geom_boxplot() +
+  facet_grid(. ~ ligand) +
+  scale_color_brewer(palette = "Set1", guide = FALSE)
 
+reference <- filter(my_df, ligand == "None")$mean_fluorescence
 
-Fold_change <- Both_plates
+my_df <- my_df %>%
+  dplyr::group_by(ligand) %>%
+  dplyr::mutate(
+    fc = mean_fluorescence / reference
+  )
 
-Fold_change[1:192, 'No_ligand'] <- Both_plates$No_ligand / Both_plates$No_ligand
-Fold_change[1:192, 'VAC'] <- Both_plates$VAC / Both_plates$No_ligand
-Fold_change[1:192, 'Vanillin'] <- Both_plates$Vanillin / Both_plates$No_ligand
-
-myplot_2 <- gather(Fold_change,  No_ligand, VAC, Vanillin, key = 'ligand', value = 'induction' ) #myplot_2 is all fold-induction data
-myplot_2 <- ggplot(myplot_2, aes(x= Well, color = ligand))
-myplot_2 + geom_point(aes(y= induction), size = 3) + facet_grid(. ~ ligand)
-
-
-df_sorted <- arrange(Fold_change, -desc(VAC))
-df_sorted$Well <- factor(df_sorted$Well, levels = unique(df_sorted$Well))
-
-myplot_3 <- gather(df_sorted,  No_ligand, VAC, Vanillin, key = 'ligand', value = 'induction' )
-myplot_3$Well <- factor(myplot_3$Well, levels = unique(myplot_3$Well))
-
-myplot_3 <- ggplot(myplot_3, aes(x= Well, y= induction, color = interaction(ligand, Type)))
-myplot_3 + geom_point() + facet_grid(. ~ ligand)
-
-
-myplot_3_b <- ggplot(df_sorted,aes(x = Well, y = VAC, color = Type))
-myplot_3_b + geom_point( size = 4)
-
-top_variants <- filter(df_sorted, VAC < 1.45)
-top_variants[29:30, ] <- Fold_change[95:96,]  
-top_variants[31:32, ] <- Fold_change[191:192,] 
-
-top_variants <- arrange(top_variants, -desc(Vanillin))
-
-myplot_4 <- gather(top_variants, VAC, Vanillin, key = 'ligand', value = 'induction' )
-myplot_4$Well <- factor(myplot_4$Well, levels = unique(myplot_4$Well))
- 
-myplot_4 <- ggplot(myplot_4, aes(x= Well, y= induction, color = interaction (ligand, Type )))
-myplot_4 + geom_point(size = 4 ) + facet_grid(. ~ ligand) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-top_variants
+ggplot(my_df, aes(x = fc,
+                  fill = type)) +
+  geom_histogram(position = "dodge") +
+  facet_grid(. ~ ligand) +
+  scale_fill_brewer(palette = "Set1")
